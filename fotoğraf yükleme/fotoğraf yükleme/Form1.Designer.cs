3529e8f66fa1b8cb3751911743e8bea2010d52c2
Ferhat Arıkoğlu <34ferhatarikoglu34@gmail.com>
2022-11-25 10:16:58 +0300

    c# ile veritabanına resim yükleme
    veritabanı bağlantınızı kendinize göre ayarlayınız lütfen

---
+++ fotoğraf yükleme/fotoğraf yükleme/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace fotoğraf_yükleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnsec = new System.Windows.Forms.Button();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.resim = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(21, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(391, 193);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnsec
+            // 
+            this.btnsec.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsec.Location = new System.Drawing.Point(418, 12);
+            this.btnsec.Name = "btnsec";
+            this.btnsec.Size = new System.Drawing.Size(58, 44);
+            this.btnsec.TabIndex = 1;
+            this.btnsec.Text = "...";
+            this.btnsec.UseVisualStyleBackColor = true;
+            this.btnsec.Click += new System.EventHandler(this.btnsec_Click);
+            // 
+            // btnekle
+            // 
+            this.btnekle.Location = new System.Drawing.Point(418, 71);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(75, 23);
+            this.btnekle.TabIndex = 2;
+            this.btnekle.Text = "ekle";
+            this.btnekle.UseVisualStyleBackColor = true;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.resim});
+            this.dataGridView1.Location = new System.Drawing.Point(533, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 150;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(593, 479);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "id";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.Width = 125;
+            // 
+            // resim
+            // 
+            this.resim.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.resim.DataPropertyName = "images";
+            this.resim.HeaderText = "resim";
+            this.resim.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.resim.MinimumWidth = 6;
+            this.resim.Name = "resim";
+            this.resim.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.resim.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 503);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnekle);
+            this.Controls.Add(this.btnsec);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnsec;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewImageColumn resim;
+    }
+}
+
---
+++ fotoğraf yükleme/fotoğraf yükleme/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+
+namespace fotoğraf_yükleme
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-DHVOIQK\\SQLEXPRESS;Initial Catalog=images;Integrated Security=True; User Id=sa;Password=19830126;");
+        OpenFileDialog file = new OpenFileDialog();
+        DataTable dataTable= new DataTable();
+        string imagepath;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlDataAdapter adtr= new SqlDataAdapter("select * from images",baglanti);
+            adtr.Fill(dataTable);
+            dataGridView1.DataSource=dataTable;
+            baglanti.Close();
+
+            dataGridView1.RowTemplate.Height = 70;
+        }
+
+        private void btnsec_Click(object sender, EventArgs e)
+        {
+            file.Title = "resim seç";
+            //file.Filter = "Jpeg Dosyaları(*.jpeg)|*.jpeg| Png Dosyaları(*.png)|*.png| Gif Dosyaları(*.gif)|*.gif| Tif Dosyaları(*.tif)|*.tif|";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(file.FileName);
+                imagepath = file.FileName;
+            }
+        }
+
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fileStream = new FileStream(imagepath, FileMode.Open, FileAccess.Read);
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                byte[] resim = binaryReader.ReadBytes((int)fileStream.Length);
+                binaryReader.Close();
+                fileStream.Close();
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("insert into images(images) values(@images)", baglanti);
+                komut.Parameters.Add("@images", SqlDbType.Image, resim.Length).Value = resim;
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("işlem başarılı");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("lütfen fotoğraf seçiniz");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select * from images where id=" +
+                "'" + int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString()) + "'",baglanti);
+            SqlDataReader dr=komut.ExecuteReader();
+            if (dr.Read())
+            {
+                if (dr["images"]!=null)
+                {
+                    byte[] resim = new byte[0];
+                    resim = (byte[])dr["images"];
+                    MemoryStream memoryStream=new MemoryStream(resim);
+                    pictureBox1.Image = Image.FromStream(memoryStream);
+                    dr.Close(); 
+                    komut.Dispose();
+                    baglanti.Close() ;  
+                }
+            }
+            baglanti.Close();   
+        }
+    }
+}
